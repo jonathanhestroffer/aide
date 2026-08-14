43411f906272bb87cde651406ed40b4ffcc84513
jonathanhestroffer <jonathanhestroffer@ucsb.edu>
2026-08-14 15:48:12 -0700

    refactor, remove IdentityComponent

--- src/aide/core/components.py
+++ src/aide/core/components.py
@@ -6,38 +6,20 @@ from torch.utils.data import Dataset
 
 
 class Component(nn.Module, ABC):
-    """
-    Base class for all components in the ML platform.
-    """
+    """Base class for all components in the ML platform."""
 
     @abstractmethod
     def forward(self, *args, **kwargs) -> Any:
-        """
-        Forward pass of the component.
-        """
+        """Forward pass of the component."""
         raise NotImplementedError("Subclasses must implement the forward method.")
 
 
 class TransformComponent(Component):
-    """
-    A specialized component that transforms data before configuring DataLoaders.
+    """A specialized component that transforms data before configuring DataLoaders.
+
     Must return a Dataset or a list of Datasets in the forward pass.
     """
 
     def forward(self, dataset: Dataset | list[Dataset]) -> Dataset | list[Dataset]:
-        """
-        Transform the dataset.
-        """
+        """Transform the dataset."""
         ...
-
-
-class Identity(Component):
-    """
-    A component that performs the identity operation.
-    """
-
-    def forward(self, x: Any, *args, **kwargs) -> Any:
-        """
-        Forward pass of the component.
-        """
-        return x
